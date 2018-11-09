d8b1d3d7ec7058a785558ec378f4af90eaf2455e
yousseff97 <32313922+yousseff97@users.noreply.github.com>
2018-11-09 16:44:24 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# learn-react
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+/*
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+*/
+
+function Affiche(props) {
+  return React.createElement("button", { type: "submit" }, "llllllll");
+}
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(<Affiche label="aaaaaaaa" />, rootElement);
